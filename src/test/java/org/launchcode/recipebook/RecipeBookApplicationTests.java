9560acdd30e7d69bf6132a0801edb8ba87cb55cd
[Mark Rheinheimer] <markrheinheimer@protonmail.comgit config --global user.name markrheinheimer>
2023-11-29 14:54:46 -0600

    project structure started

---
+++ src/test/java/org/launchcode/recipebook/RecipeBookApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.recipebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
