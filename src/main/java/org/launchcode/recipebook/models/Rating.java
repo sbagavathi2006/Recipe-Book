c957c2d8e6def70df91464a49eeb8d14bdd43ea6
William R <wrowe31@yahoo.com>
2023-12-07 20:18:14 -0600

    Rating

--- src/main/java/org/launchcode/recipebook/models/Rating.java
+++ src/main/java/org/launchcode/recipebook/models/Rating.java
@@ -13,4 +13,12 @@ public class Rating extends AbstractEntity{
 
     private double averageRating;
     private int totalVotes;
+
+    public Rating(User user, Recipe recipe, double averageRating, int totalVotes) {
+        this.user = user;
+        this.recipe = recipe;
+        this.averageRating = averageRating;
+        this.totalVotes = totalVotes;
+    }
+
 }
