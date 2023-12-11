e38678f58a2d1b3cc8e83f377d6ad6214b05b0b4
Bagavathi <sbagavathi2006@gmail.com>
2023-12-11 14:48:51 -0600

    Implimented fields for Add Recipe ,View Recipe and Index Recipe

--- src/main/java/org/launchcode/recipebook/controllers/AddRecipeController.java
+++ src/main/java/org/launchcode/recipebook/controllers/AddRecipeController.java
@@ -1,10 +1,14 @@
 package org.launchcode.recipebook.controllers;
+import jakarta.validation.Valid;
+import org.launchcode.recipebook.models.AddRecipe;
 import org.launchcode.recipebook.models.data.AddRecipeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("add-recipe")
@@ -17,10 +21,32 @@ public class AddRecipeController {
         return "add-recipe/index";
     }
 
-//    @GetMapping("add")
-//    public String displayAddRecipeForm(Model model) {
-//        model.addAttribute(new Recipe());
-//        return "add-recipe/add";
-//    }
+    @GetMapping("add")
+    public String displayAddRecipeForm(Model model) {
+        model.addAttribute(new AddRecipe());
+        return "add-recipe/add";
+    }
 
+    @PostMapping("add")
+    public String processAddRecipeForm(@ModelAttribute @Valid AddRecipe newAddRecipe,
+                                       Errors errors, Model model) {
+        if (errors.hasErrors()) {
+            return "add-recipe/add";
+        }
+        addRecipeRepository.save(newAddRecipe);
+        return "redirect:";
+    }
+    @GetMapping("view/{recipeId}")
+    public String displayViewSkill(Model model, @PathVariable int recipeId) {
+
+        Optional optSkill = addRecipeRepository.findById(recipeId);
+        if (optSkill.isPresent()) {
+            AddRecipe addRecipe = (AddRecipe) optSkill.get();
+            model.addAttribute("addRecipe", addRecipe);
+            return "add-recipe/view";
+        } else {
+            return "redirect:../";
+        }
+
+    }
 }
--- src/main/java/org/launchcode/recipebook/models/AddRecipe.java
+++ src/main/java/org/launchcode/recipebook/models/AddRecipe.java
@@ -1,7 +1,84 @@
 package org.launchcode.recipebook.models;
 
 import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class AddRecipe extends AbstractEntity{
+    @NotBlank(message = "Recipe Name is required")
+    @Size(min=2, max = 75, message = "Name must be between 2 to 75 characters")
+    private String recipeName;
+
+//    @NotBlank(message = "Ingredient is required")
+//    @Size(min =2, max = 250, message = "Name should be between 2 to 250 characters")
+//    private List<String> ingredients = new ArrayList<>();
+
+    @NotBlank(message = "Preparation is required")
+    @Size(min =2, max = 1500, message = "Name should be between 2 to 1500 characters")
+    private String preparation;
+
+    @NotBlank(message = "Meal Type is required")
+    private String mealType;
+
+    @NotBlank(message = "Creator Name is required")
+    @Size(min=2, max = 75, message = "Name must be between 2 to 75 characters")
+    private String creatorName;
+
+    @NotBlank(message = "Image is required")
+    private String imageUrl;
+
+    public AddRecipe(){   }
+
+    public String getRecipeName() {
+        return recipeName;
+    }
+
+    public void setRecipeName(String recipeName) {
+        this.recipeName = recipeName;
+    }
+
+//    public List<String> getIngredients() {
+//        return ingredients;
+//    }
+//
+//    public void setIngredients(List<String> ingredients) {
+//        this.ingredients = ingredients;
+//    }
+
+    public String getPreparation() {
+        return preparation;
+    }
+
+    public void setPreparation(String preparation) {
+        this.preparation = preparation;
+    }
+
+    public String getMealType() {
+        return mealType;
+    }
+
+    public void setMealType(String mealType) {
+        this.mealType = mealType;
+    }
+
+    public String getCreatorName() {
+        return creatorName;
+    }
+
+    public void setCreatorName(String creatorName) {
+        this.creatorName = creatorName;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
 }
