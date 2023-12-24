e4b7eddb1cddfcc71a74f851d2697616eb99795c
rheins22 <127905208+rheins22@users.noreply.github.com>
2023-12-24 17:21:31 -0600

    started working on get recipe list

--- frontend/src/components/FavoriteRecipesComponent.jsx
+++ frontend/src/components/FavoriteRecipesComponent.jsx
@@ -1,4 +1,27 @@
-export default function FavoriteRecipesComponent(recipeList) {
+import {useState, useEffect} from 'React';
+
+export default function FavoriteRecipesComponent() {
+    const [recipeList, setRecipeList] = useState(null);
+
+    useEffect() => {
+    function handleClick(recipeData) {
+        fetch('http://localhost:8080/add-recipe/get', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(recipeData),
+        })
+          .then((response) => {
+            console.log(response.text());
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
+
+    }, [recipeList];
+
   return (
     <div className="favoriteRecipes">
       <h2>Favorite Recipes</h2>
