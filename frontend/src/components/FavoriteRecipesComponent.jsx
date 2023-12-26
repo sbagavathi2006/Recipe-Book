1adcbec0b775f4493f65d8d8bdc8865ae0c8d899
[Mark Rheinheimer] <markrheinheimer@protonmail.comgit config --global user.name markrheinheimer>
2023-12-26 09:52:08 -0600

    working on favorite recipes list and get request to addrecipecontroller

--- frontend/src/components/FavoriteRecipesComponent.jsx
+++ frontend/src/components/FavoriteRecipesComponent.jsx
@@ -1,14 +1,46 @@
-export default function FavoriteRecipesComponent(recipeList) {
+import { useEffect, useState } from 'react';
+
+export default function FavoriteRecipesComponent() {
+  const [recipeList, setRecipeList] = useState(null);
+
+  useEffect(() => {
+    function fetchData() {
+      fetch('http://localhost:8080/add-recipe/get', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+        .then((response) => {
+          if (!response.ok) {
+            throw new error('Network resonse was not ok');
+          }
+          return response.json();
+        })
+        .then((data) => {
+          console.log(data);
+          setRecipeList(data);
+        })
+        .catch((error) => {
+          console.error('There was a problem fetching the data: ', error);
+        });
+    }
+
+    fetchData();
+  }, []);
+
   return (
     <div className="favoriteRecipes">
       <h2>Favorite Recipes</h2>
-      {/* {recipeList.map((recipe, index) => (
-        <div>
-          <ul>
+      {recipeList && recipeList.length > 0 ? (
+        <ul>
+          {recipeList.map((recipe, index) => (
             <li key={index}>{recipe.title}</li>
-          </ul>
-        </div>
-      ))} */}
+          ))}
+        </ul>
+      ) : (
+        <p>No favorite recipes found</p>
+      )}
     </div>
   );
 }
