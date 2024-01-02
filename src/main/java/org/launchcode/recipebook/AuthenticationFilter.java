d0854056af9a0a34fd1069aee7a994088ab9bdf1
[Mark Rheinheimer] <markrheinheimer@protonmail.comgit config --global user.name markrheinheimer>
2024-01-02 17:55:15 -0600

    styling changes for registration and login pages

--- src/main/java/org/launchcode/recipebook/AuthenticationFilter.java
+++ src/main/java/org/launchcode/recipebook/AuthenticationFilter.java
@@ -21,9 +21,7 @@ public class AuthenticationFilter implements HandlerInterceptor {
     @Autowired
     AuthenticationController authenticationController;
 
-    private static final List<String> whitelist = Arrays.asList("/login", "/register", "/logout", "/css");
-
-
+    private static final List<String> whitelist = Arrays.asList("/static", "/login", "/register", "/logout", "/css", "/images");
 
     private static boolean isWhitelisted(String path) {
         for (String pathRoot : whitelist) {
