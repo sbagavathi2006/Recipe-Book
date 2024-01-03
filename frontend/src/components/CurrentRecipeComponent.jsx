6fe0a0e31aea2c3982eaae8fb2d5be1c1d62e990
Elango <mail2telango@gmail.com>
2024-01-02 19:40:08 -0600

    fixed upload image issues

--- frontend/src/components/CurrentRecipeComponent.jsx
+++ frontend/src/components/CurrentRecipeComponent.jsx
@@ -287,19 +287,20 @@ import PrintButton from './PrintButtonComponent.jsx';
 export default function CurrentRecipeComponent({ recipe, showAddRecipeForm, setRecipe, setShowAddRecipeForm }) {
   const [displayedRecipe, setDisplayedRecipe] = useState(null);
   const [successMessage, setSuccessMessage] = useState("");
+  const [base64Image, setBase64Image] = useState("");
 
   // Code for Add form recipe -Baga
   const [addrecipe, setAddrecipe] = useState({
     name: '',
     description: '',
     ingredients: [],
-    image: '',
+    image: null,
   });
     const[errors, setErrors] = useState({
-      name: "",
-      description: "",
+      name: '',
+      description: '',
       ingredients: [],
-      image: ""
+      image: null,
     })
   const { name, description, ingredients, image } = addrecipe;
 
@@ -320,6 +321,19 @@ export default function CurrentRecipeComponent({ recipe, showAddRecipeForm, setR
         setErrors({ ...errors, [name]: '' });
   };
 
+  const onFileChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+        reader.onloadend = () => {
+        setAddrecipe({ ...addrecipe, image: reader.result });
+        setErrors({ ...errors, image: null });
+      };
+        reader.readAsDataURL(file);
+    }
+  };
+
+
   const onSubmit = async (e) => {
     e.preventDefault();
         //Basic client side validations
@@ -333,20 +347,26 @@ export default function CurrentRecipeComponent({ recipe, showAddRecipeForm, setR
         if(!ingredients.length){
           newErrors = {...newErrors, ingredients: "Please enter recipe ingredients."}
         }
-        if(!image.trim()){
-          newErrors = {...newErrors, image: "Please enter recipe image url."}
+        if(!image){
+          newErrors = {...newErrors, image: "Please upload recipe image."}
         }
         // If there are errors, update the state and stop the submission
         if(Object.keys(newErrors).length>0){
           setErrors(newErrors);
           return;
         }
+        console.log(addrecipe);
 
     try {
       const response = await fetch('http://localhost:8080/add-recipe/add', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(addrecipe),
+        body: JSON.stringify({
+          name,
+          description,
+          ingredients,
+          image,
+        }),
       });
       if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
@@ -359,7 +379,7 @@ export default function CurrentRecipeComponent({ recipe, showAddRecipeForm, setR
         name: '',
         description: '',
         ingredients: [],
-        image: '',
+        image: null,
     });
 
     } catch (error) {
@@ -425,7 +445,7 @@ export default function CurrentRecipeComponent({ recipe, showAddRecipeForm, setR
       recipe?.description
     ) {
       let { name, image, ingredients, description } = recipe;
-
+      
       // Build out displayedRecipe object to update displayed content
       const updatedDisplayedRecipe = {
         name: name,
@@ -485,14 +505,14 @@ export default function CurrentRecipeComponent({ recipe, showAddRecipeForm, setR
           <div className="error-message">{errors.ingredients}</div>
           <br></br>
           <div>
-            <label htmlFor="recipe-image"> Image URL : </label>
+            <label htmlFor="recipe-image-upload"> Image Upload : </label>
             <input
-              type="text"
-              id="recipe-image"
+              type="file"
+              id="recipe-image-upload"
               name="image"
-              placeholder="Upload your recipe's image"
-              onChange={(e) => onInputChange(e)}
-              value={image}
+              // placeholder="Upload your recipe's image"
+              onChange={(e) => onFileChange(e)}
+              // value={image}
             />
             <div className="error-message">{errors.image}</div>
           </div>
