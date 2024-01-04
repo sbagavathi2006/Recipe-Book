1250a28b3a34b24a116c6d8b12878b6d61cba68a
Elango <sbagavathi2006@gmail.com>
2024-01-04 15:09:24 -0600

    upload image done

--- frontend/src/components/CurrentRecipeComponent.jsx
+++ frontend/src/components/CurrentRecipeComponent.jsx
@@ -1,284 +1,3 @@
-// import { useState, useEffect } from 'react';
-// import PrintButton from './PrintButtonComponent.jsx';
-//
-// export default function CurrentRecipeComponent({ recipe, showAddRecipeForm, setRecipe, setShowAddRecipeForm }) {
-//   const [displayedRecipe, setDisplayedRecipe] = useState(null);
-//   const [successMessage, setSuccessMessage] = useState("");
-//
-//   const [addrecipe, setAddrecipe] = useState({
-//     name: '',
-//     description: '',
-//     ingredients: [],
-//     image: null,
-//   });
-//     const[errors, setErrors] = useState({
-//       name: "",
-//       description: "",
-//       ingredients: [],
-//       image: null,
-//     })
-//   const { name, description, ingredients, image } = addrecipe;
-//
-//   const onInputChange = (e) => {
-//     const { name, value } = e.target;
-//         if (name === 'ingredients') {
-//           setAddrecipe({
-//             ...addrecipe,
-//             [name]: value.split('\n').map((ingredient) => ingredient.trim()),
-//           });
-//         } else {
-//           // For other fields, update as usual
-//           setAddrecipe({ ...addrecipe, [name]: value });
-//         }
-//
-//         // Clear the associated error when the user starts typing
-//         setErrors({ ...errors, [name]: '' });
-//     };
-//
-//   };
-// //convert image file to url
-//   const onFileChange = (e) => {
-//     const file = e.target.files[0];
-//     if (file) {
-//       const reader = new FileReader();
-//         reader.onloadend = () => {
-//         setAddrecipe({ ...addrecipe, image: reader.result });
-//         setErrors({ ...errors, image: null });
-//       };
-//         reader.readAsDataURL(file);
-//     }
-//
-//   const onSubmit = async (e) => {
-//     e.preventDefault();
-//         //Basic client side validations
-//         let newErrors ={};
-//         if(!name.trim()){
-//           newErrors = {...newErrors, name: "Enter recipe title."}
-//         }
-//         if(!description.trim()){
-//           newErrors = {...newErrors, description: "Enter recipe description."}
-//         }
-//         if(!ingredients.length){
-//           newErrors = {...newErrors, ingredients: "Enter recipe ingredients."}
-//         }
-//         if(!image){
-//           newErrors = {...newErrors, image: "Upload recipe image."}
-//         }
-//         // If there are errors, update the state and stop the submission
-//         if(Object.keys(newErrors).length>0){
-//           setErrors(newErrors);
-//           return;
-//         }
-//
-//     try {
-//        const response = await fetch('http://localhost:8080/add-recipe/add', {
-//         method: 'POST',
-//         headers: { 'Content-Type': 'application/json' },
-//         body: JSON.stringify({
-//           name,
-//           description,
-//           ingredients,
-//           image,
-//         }),
-//       });
-//
-//       if (!response.ok) {
-//         throw new Error(`HTTP error! Status: ${response.status}`);
-//       }
-//
-//       setSuccessMessage("Recipe added successfully!!");
-//
-//       // Reset the form fields to an empty state
-//       setAddrecipe({
-//         name: '',
-//         description: '',
-//         ingredients: [],
-//         image: null,
-//     });
-//
-//     } catch (error) {
-//       console.error('Error during form submission:', error);
-//     }
-//   };
-//
-//   const handleClick = (recipeData) => {
-//     console.log(recipeData);
-//     fetch('http://localhost:8080/add-recipe/add', {
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/json',
-//       },
-//       body: JSON.stringify(recipeData),
-//     })
-//       .then((response) => {
-//       if (!response.ok) {
-//       throw new error('Network response was not ok')}
-//       return response.json();
-//       })
-//       .then((data) => {
-//         console.log(data);
-//         setRecipe(data);
-//       })
-//       .catch((error) => {
-//         console.error('Error adding the recipe: ', error);
-//       });
-//   };
-//
-//   useEffect(() => {
-//     if (
-//       recipe?.title &&
-//       recipe?.image &&
-//       recipe?.extendedIngredients &&
-//       recipe?.plainTextInstructions
-//     ) {
-//       let { title, image, extendedIngredients, plainTextInstructions } = recipe;
-//
-//       // Shortens instructions to fit screen/not throw an error saving to database
-//       plainTextInstructions = plainTextInstructions.slice(0, 900);
-//
-//       // Build out displayedRecipe object to update displayed content
-//       const updatedDisplayedRecipe = {
-//         name: title,
-//         description: plainTextInstructions,
-//         ingredients: extendedIngredients.map(
-//           (ingredient) => ingredient.original
-//         ),
-//         image: image,
-//       };
-//
-//       setDisplayedRecipe(updatedDisplayedRecipe);
-//     }
-//   }, [recipe]);
-//
-//   useEffect(() => {
-//     console.log(recipe);
-//     if (
-//       recipe?.name &&
-//       recipe?.image &&
-//       recipe?.ingredients &&
-//       recipe?.description
-//     ) {
-//       let { name, image, ingredients, description } = recipe;
-//
-//       // Build out displayedRecipe object to update displayed content
-//       const updatedDisplayedRecipe = {
-//         name: name,
-//         description: description,
-//         ingredients: ingredients.map((ingredient) => ingredient),
-//         image: image,
-//       };
-//
-//       setDisplayedRecipe(updatedDisplayedRecipe);
-//     }
-//   }, [recipe]);
-//
-//   if (showAddRecipeForm === true) {
-//     return (
-//       <div className="currentRecipe">
-//          {successMessage && <div className="success-message">{successMessage}</div>}
-//
-//         <h1>Add Your Own Recipe!!!</h1>
-//         <form onSubmit={(e) => onSubmit(e)} className="form-group">
-//           <div>
-//             <label htmlFor="recipe-name"> Title : </label>
-//             <input
-//               type="text"
-//               id="recipe-name"
-//               name="name"
-//               placeholder="Your recipe's title"
-//               onChange={(e) => onInputChange(e)}
-//               value={name}
-//             />
-//             <div className="error-message">{errors.name}</div>
-//           </div>
-//           <br></br>
-//           <div>
-//             <label htmlFor="recipe-description"> Description : </label>
-//             <textarea
-//               type="text"
-//               id="recipe-description"
-//               name="description"
-//               placeholder="Your recipe's instructions"
-//               onChange={(e) => onInputChange(e)}
-//               value={description}
-//             />
-//             <div className="error-message">{errors.description}</div>
-//           </div>
-//           <br></br>
-//           <div>
-//             <label htmlFor="recipe-ingredients"> Ingredients : </label>
-//             <textarea
-//               type="text"
-//               id="recipe-ingredients"
-//               name="ingredients"
-//               placeholder="Your recipe's ingredients"
-//               onChange={(e) => onInputChange(e)}
-//               value={ingredients.join('\n')}
-//             />
-//           </div>
-//           <div className="error-message">{errors.ingredients}</div>
-//           <br></br>
-//           <div>
-//             <label htmlFor="recipe-image-upload"> Image Upload : </label>
-//             <input
-//               type="file"
-//               id="recipe-image-upload"
-//               name="image"
-//               // placeholder="Upload recipe image"
-//               onChange={(e) => onFileChange(e)}
-//               // value={image}
-//             />
-//             <div className="error-message">{errors.image}</div>
-//           </div>
-//           <br></br>
-//           <button type="submit">Add Recipe</button>
-//           {/* <button type="reset"> Reset Form</button> */}
-//         </form>
-//       </div>
-//     );
-//   } else if (!displayedRecipe) {
-//     return (
-//       <div className="currentRecipe">
-//         <p>Select a recipe</p>
-//       </div>
-//     );
-//   } else {
-//     const { name, description, ingredients, image } = displayedRecipe;
-//     return (
-//       <div className="currentRecipe">
-//         <div className="current-title-favorite">
-//           <p>
-//             {name && name + ' '}
-//             <button
-//               className="favorite-btn"
-//               onClick={() => handleClick(displayedRecipe)}
-//             >
-//               <span className="star"></span> Add to Favorites
-//             </button>
-//           </p>
-//         </div>
-//         <div>
-//           <div className="current-image-div">
-//             <img src={image} alt={name} className="current-image" />
-//           </div>
-//           <div id="recipeToPrint">
-//             <h3>Ingredients List:</h3>
-//             <ul>
-//               {ingredients.map((ingredient, index) => (
-//                 <li key={index}>{ingredient}</li>
-//               ))}
-//             </ul>
-//             <p>{description}</p>
-//           </div>
-//           <PrintButton currentRecipeId="recipeToPrint" />
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 import { useState, useEffect } from 'react';
 import PrintButton from './PrintButtonComponent.jsx';
 
@@ -296,13 +15,13 @@ export default function CurrentRecipeComponent({
     name: '',
     description: '',
     ingredients: [],
-    image: '',
+    image: null,
   });
   const [errors, setErrors] = useState({
     name: '',
     description: '',
     ingredients: [],
-    image: '',
+    image: null,
   });
   const { name, description, ingredients, image } = addrecipe;
 
@@ -323,6 +42,19 @@ export default function CurrentRecipeComponent({
     setErrors({ ...errors, [name]: '' });
   };
 
+  const onFileChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+        reader.onloadend = () => {
+        setAddrecipe({ ...addrecipe, image: reader.result });
+        setErrors({ ...errors, image: null });
+      };
+        reader.readAsDataURL(file);
+    }
+  };
+
+
   const onSubmit = async (e) => {
     e.preventDefault();
     //Basic client side validations
@@ -342,7 +74,7 @@ export default function CurrentRecipeComponent({
         ingredients: 'Please enter recipe ingredients.',
       };
     }
-    if (!image.trim()) {
+    if (!image) {
       newErrors = { ...newErrors, image: 'Please enter recipe image url.' };
     }
     // If there are errors, update the state and stop the submission
@@ -360,6 +92,12 @@ export default function CurrentRecipeComponent({
       const response = await fetch('http://localhost:8080/add-recipe/add', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
+        // body: JSON.stringify({
+        //   name,
+        //   description,
+        //   ingredients,
+        //   image,
+        // }),
         body: JSON.stringify(recipeToAdd),
       });
       if (!response.ok) {
@@ -373,7 +111,7 @@ export default function CurrentRecipeComponent({
         name: '',
         description: '',
         ingredients: [],
-        image: '',
+        image: null,
       });
     } catch (error) {
       console.error('Error during form submission:', error);
@@ -509,12 +247,12 @@ export default function CurrentRecipeComponent({
           <div>
             <label htmlFor="recipe-image"> Image URL : </label>
             <input
-              type="text"
+              type="file"
               id="recipe-image"
               name="image"
-              placeholder="Upload your recipe's image"
-              onChange={(e) => onInputChange(e)}
-              value={image}
+              // placeholder="Upload your recipe's image"
+              onChange={(e) => onFileChange(e)}
+              // value={image}
             />
             <div className="error-message">{errors.image}</div>
           </div>
