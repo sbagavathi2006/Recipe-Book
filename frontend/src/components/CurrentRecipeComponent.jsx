1e580a3d46e818ca3d41161863bae46aafc238cb
Natasha <nhodg2@gmail.com>
2024-01-08 12:07:48 -0600

    Add shopping list button

--- frontend/src/components/CurrentRecipeComponent.jsx
+++ frontend/src/components/CurrentRecipeComponent.jsx
@@ -19,6 +19,10 @@ export default function CurrentRecipeComponent({
     }
   }
 
+  const handleShoppingClick = (shoppingList) => {
+    console.log(shoppingList);
+  }
+
   const clearShoppingList = () => {
     setShoppingList([]);
   }
@@ -293,6 +297,11 @@ export default function CurrentRecipeComponent({
               onClick={() => handleClick(displayedRecipe)}
             >
               <span className="star"></span> Add to Favorites
+            </button>
+            <button
+              className="shopping-btn"
+              onClick={() => handleShoppingClick(shoppingList)}>
+                Shopping List
             </button>
           </p>
         </div>
