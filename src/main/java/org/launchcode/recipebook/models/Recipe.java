bdc3198a6b9b0c41f0dc01651fda62cdceeca213
sbagavathi2006 <sbagavathi2006@gmail.com>
2024-01-15 11:24:23 -0600

    Fixed Add Recipe Image too long issue

--- src/main/java/org/launchcode/recipebook/models/Recipe.java
+++ src/main/java/org/launchcode/recipebook/models/Recipe.java
@@ -22,7 +22,7 @@ public class Recipe extends AbstractEntity {
 
 
     @NotNull
-    @Column(columnDefinition = "VARBINARY(1000)")
+    @Column(columnDefinition = "LONGTEXT")
     private String image;
 
     @NotNull
